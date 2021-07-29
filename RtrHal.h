ed9712c7608c0ff9a4e325f0bec99927f43565fd
TanWaiLiang <tanwailiang97@gmail.com>
2021-07-29 09:00:34 +0800

    1st commit

---
+++ RtrHal.h
@@ -0,0 +1,43 @@
+//Input Pin
+#define SW4 PA14
+#define SW5 PA13
+
+//Output Pin
+#define LED_BOARD PC13
+
+//PWM pin
+#define PWM_M1 PA1
+#define PWM_M2 PA3
+#define PWM_M3 PA2
+#define PWM_SERVO PA0
+
+//Analog Pin
+#define CURR_M1 PA4
+#define CURR_M2 PA5
+#define CURR_M3 PB0
+
+//Shift Register Pin
+#define LED_GRN 7
+#define LED_RED 6
+#define DIR_M1A 4
+#define DIR_M1B 5
+#define DIR_M2A 3
+#define DIR_M2B 2
+#define DIR_M3A 1
+#define DIR_M3B 0
+
+//Mux Pin
+#define SW1 5
+#define SW2 7
+#define SW3 6
+#define SW6 4
+#define SW7 2
+#define SW8 3
+#define SW9 1
+#define SW10 0
+
+
+
+
+
+
